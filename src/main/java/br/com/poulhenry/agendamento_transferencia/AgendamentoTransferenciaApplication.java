5482cb4905c109418ebf7827accf6a5472ec447e
Paulo <pholiveira2020@gmail.com>
2026-06-20 11:42:48 -0300

    feat: criado estrutura inicial do projeto

---
+++ src/main/java/br/com/poulhenry/agendamento_transferencia/AgendamentoTransferenciaApplication.java
@@ -0,0 +1,13 @@
+package br.com.poulhenry.agendamento_transferencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoTransferenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoTransferenciaApplication.class, args);
+	}
+
+}
